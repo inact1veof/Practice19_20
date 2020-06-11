f82069915c52306c5f7642a85b0aabb1ee6067dd
inact1veof <inact1veof_iachernitsin@users.noreply.github.com>
2020-06-12 01:28:33 +0500

    addTask3

---
+++ task 3/task 3/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace task_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.InputX = new System.Windows.Forms.TextBox();
+            this.InputY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 115);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Данная программа проверяет вхождение введенной точки в диапазон";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите x:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(15, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Введите y:";
+            // 
+            // InputX
+            // 
+            this.InputX.Location = new System.Drawing.Point(106, 183);
+            this.InputX.Name = "InputX";
+            this.InputX.Size = new System.Drawing.Size(199, 20);
+            this.InputX.TabIndex = 3;
+            this.InputX.TextChanged += new System.EventHandler(this.InputX_TextChanged);
+            this.InputX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputX_KeyPress);
+            // 
+            // InputY
+            // 
+            this.InputY.Location = new System.Drawing.Point(106, 214);
+            this.InputY.Name = "InputY";
+            this.InputY.Size = new System.Drawing.Size(199, 20);
+            this.InputY.TabIndex = 4;
+            this.InputY.TextChanged += new System.EventHandler(this.InputY_TextChanged);
+            this.InputY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputY_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(291, 55);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Результат: функция u. Если точка входит, то u = 0, а если нет, то u = x.";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(15, 241);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(292, 23);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "Проверить";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(17, 282);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(88, 18);
+            this.label.TabIndex = 7;
+            this.label.Text = "Результат: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(116, 282);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 19);
+            this.label5.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::task_3.Properties.Resources.dip;
+            this.pictureBox1.Location = new System.Drawing.Point(147, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 320);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.InputY);
+            this.Controls.Add(this.InputX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Попадание точки в диапазон";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox InputX;
+        private System.Windows.Forms.TextBox InputY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ task 3/task 3/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_3
+{
+    public partial class Form1 : Form
+    {
+        public static double x = 0;
+        public static double y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InputX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9') || e.KeyChar == 8)
+            {
+                return;
+            }
+
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+
+            if (e.KeyChar == ',')
+            {
+                if (InputX.Text.IndexOf(',') != -1)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void InputY_TextChanged(object sender, EventArgs e)
+        {
+            if (InputY.Text != "" && InputX.Text != "")
+            {
+                buttonStart.Enabled = true;
+            }
+            else buttonStart.Enabled = false;
+        }
+
+        private void InputY_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9') || e.KeyChar == 8)
+            {
+                return;
+            }
+
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+
+            if (e.KeyChar == ',')
+            {
+                if (InputY.Text.IndexOf(',') != -1)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void InputX_TextChanged(object sender, EventArgs e)
+        {
+            if (InputY.Text != "" && InputX.Text != "")
+            {
+                buttonStart.Enabled = true;
+            }
+            else buttonStart.Enabled = false;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToDouble(InputX.Text);
+            y = Convert.ToDouble(InputY.Text);
+            bool ok = false;
+            double result = 0;
+            if (y >= 0 && x * x + y * y <= 4 && x * x + y * y >= 1) ok = true;
+            else ok = false;
+            if (ok) result = 0;
+            else result = x;
+            string res = $"u = {result}";
+            label5.Text = res;
+        }
+    }
+}
