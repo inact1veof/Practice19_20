793a01971b7d0927018414b67ae2b849e3b59600
inact1veof <inact1veof_iachernitsin@users.noreply.github.com>
2020-06-12 00:32:10 +0500

    addTask1

---
+++ task 1/task 1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserChoice = new System.Windows.Forms.ComboBox();
+            this.ButtonInputFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputUserNumber = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OutResult = new System.Windows.Forms.Label();
+            this.ButtonSaveFile = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбор ввода:";
+            // 
+            // UserChoice
+            // 
+            this.UserChoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.UserChoice.FormattingEnabled = true;
+            this.UserChoice.Items.AddRange(new object[] {
+            "Число из файла",
+            "Ввести число в программе"});
+            this.UserChoice.Location = new System.Drawing.Point(131, 13);
+            this.UserChoice.Name = "UserChoice";
+            this.UserChoice.Size = new System.Drawing.Size(171, 21);
+            this.UserChoice.TabIndex = 1;
+            this.UserChoice.SelectedIndexChanged += new System.EventHandler(this.UserChoice_SelectedIndexChanged);
+            // 
+            // ButtonInputFile
+            // 
+            this.ButtonInputFile.Location = new System.Drawing.Point(16, 50);
+            this.ButtonInputFile.Name = "ButtonInputFile";
+            this.ButtonInputFile.Size = new System.Drawing.Size(286, 35);
+            this.ButtonInputFile.TabIndex = 2;
+            this.ButtonInputFile.Text = "Выбор файла";
+            this.ButtonInputFile.UseVisualStyleBackColor = true;
+            this.ButtonInputFile.Visible = false;
+            this.ButtonInputFile.Click += new System.EventHandler(this.ButtonInputFile_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите число:";
+            this.label2.Visible = false;
+            // 
+            // InputUserNumber
+            // 
+            this.InputUserNumber.Location = new System.Drawing.Point(133, 57);
+            this.InputUserNumber.Name = "InputUserNumber";
+            this.InputUserNumber.Size = new System.Drawing.Size(168, 20);
+            this.InputUserNumber.TabIndex = 4;
+            this.InputUserNumber.Visible = false;
+            this.InputUserNumber.TextChanged += new System.EventHandler(this.InputUserNumber_TextChanged);
+            this.InputUserNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputUserNumber_KeyPress);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(15, 91);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(286, 42);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Количество способов:";
+            // 
+            // OutResult
+            // 
+            this.OutResult.AutoSize = true;
+            this.OutResult.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OutResult.Location = new System.Drawing.Point(179, 152);
+            this.OutResult.Name = "OutResult";
+            this.OutResult.Size = new System.Drawing.Size(0, 19);
+            this.OutResult.TabIndex = 7;
+            // 
+            // ButtonSaveFile
+            // 
+            this.ButtonSaveFile.Enabled = false;
+            this.ButtonSaveFile.Location = new System.Drawing.Point(212, 139);
+            this.ButtonSaveFile.Name = "ButtonSaveFile";
+            this.ButtonSaveFile.Size = new System.Drawing.Size(90, 49);
+            this.ButtonSaveFile.TabIndex = 8;
+            this.ButtonSaveFile.Text = "Сохранить в файл";
+            this.ButtonSaveFile.UseVisualStyleBackColor = true;
+            this.ButtonSaveFile.Click += new System.EventHandler(this.ButtonSaveFile_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(130, 37);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(158, 13);
+            this.labelError.TabIndex = 9;
+            this.labelError.Text = "Некорректные данные файла";
+            this.labelError.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 198);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.ButtonSaveFile);
+            this.Controls.Add(this.OutResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.InputUserNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ButtonInputFile);
+            this.Controls.Add(this.UserChoice);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Гипотеза Гольдбаха-2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox UserChoice;
+        private System.Windows.Forms.Button ButtonInputFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputUserNumber;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label OutResult;
+        private System.Windows.Forms.Button ButtonSaveFile;
+        private System.Windows.Forms.Label labelError;
+    }
+}
+
---
+++ task 1/task 1/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace task_1
+{
+    public partial class Form1 : Form
+    {
+        public static int x = 0;
+        public static int result = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserChoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (UserChoice.SelectedIndex == 0)
+            {
+                ButtonInputFile.Visible = true;
+                label2.Visible = false;
+                InputUserNumber.Visible = false;
+            }
+            if (UserChoice.SelectedIndex == 1)
+            {
+                label2.Visible = true;
+                InputUserNumber.Visible = true;
+                ButtonInputFile.Visible = false;
+            }
+        }
+
+        private void InputUserNumber_TextChanged(object sender, EventArgs e)
+        {
+            if (InputUserNumber.Text != "") buttonStart.Enabled = true;
+            else buttonStart.Enabled = false;
+        }
+
+        private void ButtonInputFile_Click(object sender, EventArgs e)
+        {
+            labelError.Visible = false;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Данные |*.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ButtonInputFile.Enabled = false;
+                buttonStart.Enabled = true;
+                bool ok = false;
+                ok = int.TryParse(File.ReadAllText(ofd.FileName), out x);
+                if (!ok)
+                {
+                    labelError.Visible = true;
+                    buttonStart.Enabled = false;
+                    ButtonInputFile.Enabled = true;
+                }
+                else buttonStart.Enabled = true;
+            }
+            else
+            {
+                buttonStart.Enabled = false;
+            }
+        }
+
+        private void InputUserNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (UserChoice.SelectedIndex == 1)
+            {
+                x = Convert.ToInt32(InputUserNumber.Text);
+            }
+            if (x < 4 || x > 10000)
+            {
+                OutResult.Text = "0";
+            }
+            else
+            {
+                int count = 0;
+                List<int> res = new List<int>();
+                res = CreateSimpleMas(x);
+                int[] mas = new int[res.Count];
+                mas = res.ToArray();
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i; j < mas.Length; j++)
+                    {
+                        if (mas[i] + mas[j] == x) count++;
+                    }
+                }
+                OutResult.Text = count.ToString();
+                result = count;
+            }
+            ButtonSaveFile.Enabled = true;
+        }
+        private static bool isSimple(int n)
+        {
+            for (int i = 2; i <= (int)(n / 2); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static List<int> CreateSimpleMas(int n)
+        {
+            List<int> result = new List<int>();
+            for (int i = 2; i <= n; i++)
+            {
+                if (isSimple(i))
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+
+        private void ButtonSaveFile_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(fbd.SelectedPath+ "\\output.txt", result.ToString());
+            }
+        }
+    }
+}
