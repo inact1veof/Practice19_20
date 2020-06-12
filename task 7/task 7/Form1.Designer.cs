45ced0d0515bcb2ecaa328ace5229611f6b5e356
inact1veof <inact1veof_iachernitsin@users.noreply.github.com>
2020-06-13 02:52:31 +0500

    addTask7 and fix task 3

---
+++ task 7/task 7/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace task_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.InputN = new System.Windows.Forms.TextBox();
+            this.InputK = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите N:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите K:";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(15, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ответ:";
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.Location = new System.Drawing.Point(15, 156);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(287, 186);
+            this.listBoxResult.TabIndex = 4;
+            // 
+            // InputN
+            // 
+            this.InputN.Location = new System.Drawing.Point(108, 9);
+            this.InputN.Name = "InputN";
+            this.InputN.Size = new System.Drawing.Size(194, 20);
+            this.InputN.TabIndex = 5;
+            this.InputN.TextChanged += new System.EventHandler(this.InputN_TextChanged);
+            this.InputN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputN_KeyPress);
+            // 
+            // InputK
+            // 
+            this.InputK.Location = new System.Drawing.Point(108, 51);
+            this.InputK.Name = "InputK";
+            this.InputK.Size = new System.Drawing.Size(194, 20);
+            this.InputK.TabIndex = 6;
+            this.InputK.TextChanged += new System.EventHandler(this.InputK_TextChanged);
+            this.InputK.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputK_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(49, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Всего сочетаний: ";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(181, 359);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 19);
+            this.labelResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 400);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.InputK);
+            this.Controls.Add(this.InputN);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Генерация сочетаний из N по K";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.TextBox InputN;
+        private System.Windows.Forms.TextBox InputK;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ task 7/task 7/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InputK_TextChanged(object sender, EventArgs e)
+        {
+            if (InputK.Text != "" && InputN.Text != "")
+            {
+                button1.Enabled = true;
+            }
+            else button1.Enabled = false;
+        }
+
+        private void InputN_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void InputK_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void InputN_TextChanged(object sender, EventArgs e)
+        {
+            if (InputK.Text != "" && InputN.Text != "")
+            {
+                button1.Enabled = true;
+            }
+            else button1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBoxResult.Items.Clear();
+            labelResult.Text = "";
+            int N = Convert.ToInt32(InputN.Text);
+            int K = Convert.ToInt32(InputK.Text);
+            if (N <= 10)
+            {
+                char[] masN = new char[N];
+                char[] masK = new char[K];
+                int j = (int)'A';
+                for (int i = 0; i < N; i++)
+                {
+                    if (i <= 9) masN[i] = Char.Parse(i.ToString());
+                    else
+                    {
+                        masN[i] = (char)j;
+                        j++;
+                    }
+                }
+                for (int i = 0; i < K; i++)
+                {
+                    masK[i] = '0';
+                }
+                addRes(masK);
+                int k = K - 1;
+                int min = 1;
+                do
+                {
+                    for (int i = min; i < N; i++)
+                    {
+                        masK[k] = masN[i];
+                        addRes(masK);
+                    }
+                    for (int i = 1; i <= k; i++)
+                    {
+                        if (masK[i] == masN[N - 1])
+                        {
+                            masK[i - 1] = masN[Index(masN, masK[i - 1]) + 1];
+                            min = Index(masN, masK[i - 1]);
+                            for (int l = i; l <= k; l++)
+                                masK[l] = masN[min];
+                            break;
+                        }
+                    }
+                } while ((masK[0] != masN[N - 1]));
+                if (K > 1) addRes(masK);
+                double countRes = listBoxResult.Items.Count;
+                labelResult.Text = countRes.ToString();
+            }
+            else
+            {
+                Form2 fr = new Form2();
+                fr.ShowDialog();
+            }
+        }
+        void addRes(char[] masK)
+        {
+            string s = "";
+            for (int i = 0; i < masK.Length; i++)
+            {
+                s += masK[i] + " ";
+            }
+            listBoxResult.Items.Add(s);
+        }
+        static int Index(char[] masN, char x)
+        {
+            int i = 0;
+            while (masN[i] != x)
+            {
+                i++;
+            }
+            return i;
+        }       
+    }
+}
